0b8be2a3c91defc28578bf141c9f23a217254ebb
karan-parekh <karanparekh501@gmail.com>
2021-03-26 02:13:21 +0530

    added tests

--- src/api/models.py
+++ src/api/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.db.models.base import Model
 from django.utils import timezone
 
 
 class AbstractModel(models.Model):
 
-    name = models.CharField(max_length=100, blank=False, null=False, unique=True)
+    name = models.CharField(max_length=100, blank=False, null=False)
     duration = models.IntegerField(blank=False, null=False)
     upload_time = models.DateTimeField(default=timezone.now)
 
